08a774a9337e265b6175686c908ae87e2cde8e8e
Alex Paredes <alexparedesr95@gmail.com>
2022-01-29 22:49:26 -0600

    FEAT: Added rules and plugins to webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
+const HtmlWebpack = require('html-webpack-plugin')
+
 module.exports = {
     mode: 'development',
 
+    output: {
+        clean: true// Se limpia el dist y construye todo de nuevo.
+    },
+
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            }
+        ]
     },
 
     optimization: {},
 
-    plugins: []
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 }
